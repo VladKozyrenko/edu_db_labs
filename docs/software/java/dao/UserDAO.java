f80e14657683fbb1a208595b95602c2a96ecfcb6
Vladyslav <106406062+VladKozyrenko@users.noreply.github.com>
2025-06-21 16:14:37 +0300

    Update UserDAO.java

--- docs/software/java/dao/UserDAO.java
+++ docs/software/java/dao/UserDAO.java
@@ -1,1 +1,48 @@
+package dao;
+
+import model.User;
+import utils.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM users")) {
+
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("id"));
+                u.setName(rs.getString("name"));
+                u.setEmail(rs.getString("email"));
+                users.add(u);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public void insertUser(User user) {
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(
+                     "INSERT INTO users (name, email) VALUES (?, ?)")) {
+
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
