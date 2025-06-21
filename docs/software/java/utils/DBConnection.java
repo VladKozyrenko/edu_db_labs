8d606338b5097b89e4dc09413a4be82dd3e08ddc
Vladyslav <106406062+VladKozyrenko@users.noreply.github.com>
2025-06-21 16:13:38 +0300

    Update DBConnection.java

--- docs/software/java/utils/DBConnection.java
+++ docs/software/java/utils/DBConnection.java
@@ -1,1 +1,14 @@
+package utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/datamanagementsystem";
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password_here"; // Заміни!
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
