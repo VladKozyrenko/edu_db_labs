fbda3316398a72ae870b04b223a49b93ace05e9e
Vladyslav <106406062+VladKozyrenko@users.noreply.github.com>
2025-06-21 16:16:20 +0300

    Update ProductDAO.java

--- docs/software/java/dao/ProductDAO.java
+++ docs/software/java/dao/ProductDAO.java
@@ -1,1 +1,48 @@
+package dao;
+
+import model.Product;
+import utils.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+
+    public List<Product> getAllProducts() {
+        List<Product> products = new ArrayList<>();
+
+        try (Connection conn = DBConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM products")) {
+
+            while (rs.next()) {
+                Product p = new Product();
+                p.setId(rs.getInt("id"));
+                p.setName(rs.getString("name"));
+                p.setPrice(rs.getDouble("price"));
+                products.add(p);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return products;
+    }
+
+    public void insertProduct(Product product) {
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(
+                     "INSERT INTO products (name, price) VALUES (?, ?)")) {
+
+            stmt.setString(1, product.getName());
+            stmt.setDouble(2, product.getPrice());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
