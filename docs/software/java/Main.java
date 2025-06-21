dcbc677fbfd6a7aa0748dc0e0f31557ae69f86df
Vladyslav <106406062+VladKozyrenko@users.noreply.github.com>
2025-06-21 16:14:52 +0300

    Update Main.java

--- docs/software/java/Main.java
+++ docs/software/java/Main.java
@@ -1,1 +1,21 @@
+import dao.UserDAO;
+import model.User;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+
+        // Додати нового користувача
+        User user = new User(0, "Ivan Petrenko", "ivan@example.com");
+        userDAO.insertUser(user);
+
+        // Вивести всіх користувачів
+        List<User> users = userDAO.getAllUsers();
+        for (User u : users) {
+            System.out.println(u.getId() + " | " + u.getName() + " | " + u.getEmail());
+        }
+    }
+}
 
